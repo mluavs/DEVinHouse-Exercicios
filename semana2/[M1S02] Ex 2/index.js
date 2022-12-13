3b8a4f43f94f7dccdfbe9e2946f19eb857f794c2
mluavs <mluasantos@gmail.com>
2022-12-13 17:04:21 -0300

    add [M1S02]

---
+++ semana2/[M1S02] Ex 2/index.js
@@ -0,0 +1,9 @@
+let idade = prompt('qual sua idade?')
+
+if (idade <= 15) {
+    alert ('você é jovem')
+} else if (idade >=16 && idade <=64) {
+    alert ('você é adulto')
+} else if (idade >=65) {
+    alert ('você é idoso')
+}
