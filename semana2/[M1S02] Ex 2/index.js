193e4a0b511bffc0ae5a6c68ac084730fc6d7518
marina lua <mluasantos@gmail.com>
2022-12-20 14:48:00 -0300

    arruma html

--- semana2/[M1S02] Ex 2/index.js
+++ semana2/[M1S02] Ex 2/index.js
@@ -1,9 +1,16 @@
-let idade = prompt('qual sua idade?')
+console.log("M1S02-Ex2")
 
-if (idade <= 15) {
-    alert ('você é jovem')
+let idade = prompt('Qual a sua idade, meu bem?')
+
+if (idade === '') {
+    alert ('Não digitou nada')
+} else if (idade <= 15) {
+    alert ('Jovem!')
 } else if (idade >=16 && idade <=64) {
-    alert ('você é adulto')
+    alert ('Adulto!')
 } else if (idade >=65) {
-    alert ('você é idoso')
-}
+    alert ('Idoso!')
+} 
+
+// como prevenir a inserção de dados que não sejam números? 
+// como prevenir a inserção de números que não sejam uma idade? 
