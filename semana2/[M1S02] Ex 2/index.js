a88c3f3a3757d479fbd5ad21096d389e90e61ca7
marina lua <mluasantos@gmail.com>
2022-12-20 22:43:50 -0300

    adiciona parseInt

--- semana2/[M1S02] Ex 2/index.js
+++ semana2/[M1S02] Ex 2/index.js
@@ -1,10 +1,10 @@
 console.log("M1S02-Ex2")
 
-let idade = prompt('Qual a sua idade, meu bem?')
+let idade = parseInt(prompt('Qual a sua idade, meu bem?'))
 
 if (idade === '') {
     alert ('Não digitou nada')
-} else if (idade <= 15) {
+} else if (idade >=0 && idade <= 15) {
     alert ('Jovem!')
 } else if (idade >=16 && idade <=64) {
     alert ('Adulto!')
@@ -12,5 +12,5 @@ if (idade === '') {
     alert ('Idoso!')
 } 
 
-// como prevenir a inserção de dados que não sejam números? 
+// como prevenir a inserção de dados que não sejam números? com parseInt
 // como prevenir a inserção de números que não sejam uma idade? 
