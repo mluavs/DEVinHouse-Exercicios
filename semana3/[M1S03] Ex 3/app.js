254b8f59217073d2a96a926952a9506a751e7f00
marina lua <mluasantos@gmail.com>
2023-01-22 12:25:55 -0300

    add ex 3

---
+++ semana3/[M1S03] Ex 3/app.js
@@ -0,0 +1,13 @@
+// Resolução:
+/*
+function concatena(arrayA, arrayB) {
+  return [...arrayA, ...arrayB];
+};
+*/
+
+const concatena = (arrayA, arrayB) => [...arrayA, ...arrayB];
+
+const novoArray = concatena([1, 2, 3], [4, 5, 6]);
+
+console.log(novoArray);
+// novo array deve ser [1, 2, 3, 4, 5, 6]
