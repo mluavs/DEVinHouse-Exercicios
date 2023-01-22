dab789307930387c90b97e516d706665d7ea6607
marina lua <mluasantos@gmail.com>
2023-01-22 12:45:42 -0300

    add ex 5

---
+++ semana3/[M1S03] Ex 5/script.js
@@ -0,0 +1,18 @@
+// Resolução:
+
+
+// function somaTudo(...numeros) {
+    const somaTudo = (...numeros) => {
+        let total = 0;
+        
+        numeros.forEach((num) => {
+          total += num;
+        });
+        
+        return total;
+      }
+      
+      const resultado = somaTudo(1, 2, 3, 4);
+      
+      console.log(resultado);
+      // resultado deve ser 10
